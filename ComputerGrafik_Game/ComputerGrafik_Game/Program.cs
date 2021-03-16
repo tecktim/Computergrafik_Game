3224ee870e09ab385936888ad9986310f9435332
Tim Friedrich <tifri99@web.de>
2021-03-16 18:33:22 +0100

    now uses latest version of c# automatically

--- ComputerGrafik_Game/ComputerGrafik_Game/Program.cs
+++ ComputerGrafik_Game/ComputerGrafik_Game/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+GameWindow window = new GameWindow(GameWindowSettings.Default, new NativeWindowSettings { Profile = ContextProfile.Compatability });
+GL.ClearColor(Color4.Brown);
+float x = 0f;
+
+window.UpdateFrame += args => Update((float)args.Time);
+window.RenderFrame += _ => Draw();
+window.Run();
+
+void Update(float time)
+{
+    x += time * 0.1f;
+    window.KeyboardState.IsKeyDown(Keys.Left);
+}
+
+void Draw()
+{
+    GL.Clear(ClearBufferMask.ColorBufferBit);
+    GL.Begin(PrimitiveType.Quads);
+    GL.Vertex2(x + 0f, 0f);
+    GL.Vertex2(x + 10f, 0f);
+    GL.Vertex2(x + 1f, 1f);
+    GL.Vertex2(x + 0f, 1f);
+    GL.End();
+    window.SwapBuffers();
+}
 
 namespace ComputerGrafik_Game
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
 }
