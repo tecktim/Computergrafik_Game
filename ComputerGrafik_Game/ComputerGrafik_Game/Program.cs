0c03094d8e493c90ac67e4ee3cad9a3d568b7218
Tim Friedrich <tifri99@web.de>
2021-03-16 17:10:47 +0100

    Initial Commit Hello World

---
+++ ComputerGrafik_Game/ComputerGrafik_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerGrafik_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
